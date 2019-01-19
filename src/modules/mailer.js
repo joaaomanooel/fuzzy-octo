3ca40f24d7cfa05399555a1cb3e0727f3467e792
João Manoel Neto <joaaomanooel@gmail.com>
2019-01-19 01:00:47 -0200

    add nodemailer-express-handlebars

--- src/modules/mailer.js
+++ src/modules/mailer.js
@@ -1,5 +1,16 @@
 /* eslint-disable object-curly-newline */
 const nodemailer = require('nodemailer');
+const path = require('path');
+const hbs = require('nodemailer-express-handlebars');
+
 const { host, port, user, pass } = require('../config/mail.json');
 
-module.exports = nodemailer.createTransport({ host, port, auth: { user, pass } });
+const transport = nodemailer.createTransport({ host, port, auth: { user, pass } });
+
+transport.use('compile', hbs({
+  viewEngine: 'handlebars',
+  viewPath: path.resolve('./src/resources/mail/'),
+  extName: 'html',
+}));
+
+module.exports = transport;
